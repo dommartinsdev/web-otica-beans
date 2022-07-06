97fbc061fd16b343bfe400a6e17202ac250dbda3
Jessé Martins <jessemartins.ti@gmail.com>
2022-07-06 18:20:37 -0300

    refactor: ajustes nos links de navegação do projeto

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs')
 app.use('/', router)
 app.use('/service', router)
 app.use('/about', router)
+app.use('/contact', router)
 
 app.use(function(req, res,) {
     let title = 'Ótica Beans - Erro 404'
